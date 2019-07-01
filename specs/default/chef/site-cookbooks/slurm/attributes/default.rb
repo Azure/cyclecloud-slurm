5cb718bf7e6957fd0113123f9e5d9429dbd22945
Ben Watrous <ben.watrous@microsoft.com>
2019-07-01 12:17:02 -0700

    Set UID/GID for slurm and munge users explicitly to remove dependency on user creation order

--- specs/default/chef/site-cookbooks/slurm/attributes/default.rb
+++ specs/default/chef/site-cookbooks/slurm/attributes/default.rb
@@ -3,3 +3,8 @@
 default[:slurm][:version] = "17.11.7-1"
 default[:slurm][:arch] = "el7.centos.x86_64"
 default[:slurm][:user][:name] = 'slurm'
+default[:slurm][:user][:uid] = 19000
+default[:slurm][:user][:gid] = 19000
+default[:munge][:user][:name] = 'munge'
+default[:munge][:user][:uid] = 19001
+default[:munge][:user][:gid] = 19001
