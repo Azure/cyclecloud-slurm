65179f7c1140a03d0792246b5fc7c55431bfb5a6
Ben Watrous <ben.watrous@microsoft.com>
2020-01-28 00:31:17 -0800

    Skip package downloads, installs and version checks if slurm already installed.  (Optimal for pre-baked images.)

--- specs/default/chef/roles/slurm_execute_role.rb
+++ specs/default/chef/roles/slurm_execute_role.rb
@@ -3,7 +3,5 @@
 name "slurm_execute_role"
 description "Slurm Execute Role"
 run_list("recipe[cyclecloud]",
-  "recipe[cshared::client]",
-  "recipe[cuser]",
   "recipe[slurm::execute]",
   "recipe[cganglia::client]")
