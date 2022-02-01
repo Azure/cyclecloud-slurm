5c0d7d3e36a9f99733a306e1fa7424653e173687
Ryan Hamel <ryan.hamel@microsoft.com>
2022-02-01 15:19:15 -0500

    Fix: deallocated nodes re-register hostnames

--- specs/default/chef/site-cookbooks/slurm/recipes/execute.rb
+++ specs/default/chef/site-cookbooks/slurm/recipes/execute.rb
@@ -38,9 +38,32 @@ filenames.each do |filename|
 end
 
 if node[:slurm][:use_nodename_as_hostname] then
+  
+  execute 'unset hostname' do
+    command "hostnamectl set-hostname #{nodename}-unset#{dns_suffix}"
+    only_if "hostname | grep -qv #{nodename}-unset"
+    only_if "nslookup #{node[:ipaddress]} | grep -v #{nodename}"
+    # reboot detected
+    only_if { ::File.exist?("/etc/slurm.reenabled")}
+  end
+  
+  execute 'wait for hostname unset detection' do
+    command "nslookup #{node[:ipaddress]} | grep #{nodename}-unset"
+    # wait for waagent to notice the change
+    only_if "hostname | grep -q #{nodename}-unset"
+    retries 12
+    retry_delay 10
+  end
+  
   execute 'set hostname' do
     command "hostnamectl set-hostname #{nodename}#{dns_suffix}"
-    creates '/etc/slurm.hostname.#{nodename}.enabled'
+  end
+
+  execute 'wait for hostname detection' do
+    command "nslookup #{node[:ipaddress]} | grep #{nodename} | grep -v #{nodename}-unset"
+    only_if "hostname | grep -q #{nodename}"
+    retries 12
+    retry_delay 10
   end
 end
 
