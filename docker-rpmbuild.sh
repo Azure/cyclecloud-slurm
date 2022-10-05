a0881f442a7700fa7bf5f91fa927a534f9f04baf
Ryan Hamel <ryan.hamel@microsoft.com>
2022-10-05 12:13:11 -0400

    Remove unneeded rpm/deb validation

--- docker-rpmbuild.sh
+++ docker-rpmbuild.sh
@@ -3,18 +3,7 @@
 if command -v docker; then
 
   docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs:/root/rpmbuild/RPMS/x86_64 -ti almalinux:8.5 /bin/bash /source/00-build-slurm.sh
-  # docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs:/root/rpmbuild/RPMS/x86_64 -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
-  cat > /tmp/check_mising_files.py <<EOF
-from configparser import ConfigParser
-import os
-parser = ConfigParser()
-parser.read("project.ini")
-expected_files = set([x.strip() for x in parser.get("blobs", "Files").split(",")])
-actual = set(os.listdir("blobs"))
-missing = expected_files - actual
-assert not missing, "Missing the following blobs: %s" % ",".join(missing)
-EOF
-  python3 /tmp/check_mising_files.py
+  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs:/root/rpmbuild/RPMS/x86_64 -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
 else
   echo "`docker` binary not found. Install docker to build RPMs with this script"
 fi
