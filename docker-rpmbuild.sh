efd2b28c1092edf5dd5cf7dfed8284b842f9491d
Ryan Hamel <ryan.hamel@microsoft.com>
2022-12-12 17:08:45 -0500

    Use 22.05.8  and 23.02

--- docker-rpmbuild.sh
+++ docker-rpmbuild.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+set -e
+cd $(dirname $0)/install
+if [ -e pkg ]; then 
+  mkdir pkg
+fi
 
-SLURM22="22.05.3"
-SLURM20="20.11.9"
+TMP_BINS=slurm-pkgs-tmp
+rm -rf $TMP_BINS
+mkdir $TMP_BINS
 
-rm -rf blobs-build-tmp
-mkdir blobs-build-tmp
 if command -v docker; then
+  extra_args=
+  if command -v podman; then
+    extra_args="--privileged"
+  fi
 
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs-debian-tmp:/root/rpmbuild/RPMS/x86_64 -ti almalinux:8.5 /bin/bash /source/00-build-slurm.sh centos $SLURM22
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs-debian-tmp:/root/rpmbuild/RPMS/x86_64 -ti centos:7 /bin/bash /source/00-build-slurm.sh centos $SLURM20
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs-debian-tmp:/root/rpmbuild/RPMS/x86_64 -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
-  # we only want the rpms for almalinux8 (i.e. slurm22) but we want all of the debs
-  mv blobs-debian-tmp/*.deb blobs/
-  mv blobs-debian-tmp/*$SLURM22*.rpm blobs/
-  rm -rf blobs-debian-tmp
+  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 $extra_args -ti almalinux:8.5 /bin/bash /source/00-build-slurm.sh centos
+  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 -ti centos:7 /bin/bash /source/00-build-slurm.sh centos
+  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 $extra_args -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
+  
+  mv $TMP_BINS/* slurm-pkgs/
+  rm -rf $TMP_BINS
 else
   echo "`docker` binary not found. Install docker to build RPMs with this script"
 fi
+
+
--- specs/default/cluster-init/files/00-build-slurm.sh
+++ specs/default/cluster-init/files/00-build-slurm.sh
@@ -40,8 +40,10 @@ function build_slurm() {
 
              # munge is in EPEL
             yum -y install epel-release && yum -q makecache
-            yum install -y make $PYTHON which rpm-build munge-devel munge-libs readline-devel openssl openssl-devel pam-devel perl-ExtUtils-MakeMaker gcc mysql mysql-devel wget gtk2-devel.x86_64 glib2-devel.x86_64 $LIBTOOL m4 automake rsync
+            yum install -y make $PYTHON which rpm-build munge-devel munge-libs readline-devel openssl openssl-devel pam-devel perl-ExtUtils-MakeMaker gcc mysql mysql-devel wget gtk2-devel.x86_64 glib2-devel.x86_64 $LIBTOOL m4 automake rsync lua-devel
+            yum install -y http-parser-devel json-c-devel
             ;;
+            
     esac
 
     if [ ! -e ~/bin ]; then
@@ -62,40 +64,7 @@ function build_slurm() {
         wget "${DOWNLOAD_URL}/${SLURM_PKG}"
     fi
 
-    rpmbuild --with mysql -ta ${SLURM_PKG}
-
-    # make the plugin
-    rm -rf ~/job_submit
-    mkdir -p ~/job_submit
-    cd ~/
-    cd ~/job_submit
-    tar xjf ~/${SLURM_PKG}
-    mkdir -p ${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    cd ${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    rsync -a ${SCRIPT_DIR}/JobSubmitPlugin/ .
-
-    if [ "$SLURM_VERSION" \> "19" ]; then
-        mv Makefile.in.v19 Makefile.in
-    fi
-
-    cd ~/job_submit
-    sed -i 's/src\/plugins\/job_submit\/Makefile/src\/plugins\/job_submit\/Makefile\n                 src\/plugins\/job_submit\/cyclecloud\/Makefile/g'  ${SLURM_FOLDER}/configure.ac
-    cd ~/job_submit/${SLURM_FOLDER}
-
-    if [ "$SLURM_VERSION" \> "19" ]; then
-        autoconf
-    else
-        ./autogen.sh
-    fi
-
-    ./configure
-    make
-    cd ~/job_submit/${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    make
-
-    LD_LIBRARY_PATH=/root/job_submit/${SLURM_FOLDER}/src/api/.libs/ JOB_SUBMIT_CYCLECLOUD=1 python3 job_submit_cyclecloud_test.py
-    cp .libs/job_submit_cyclecloud.so job_submit_cyclecloud_${DISTRO_FAMILY}${CENTOS_MAJOR}_${SLURM_VERSION}-1.so
-    cp job_submit_cyclecloud_${DISTRO_FAMILY}${CENTOS_MAJOR}_${SLURM_VERSION}-1.so  /usr/src/packages/RPMS/x86_64/
+    rpmbuild --with mysql --define '_with_pmix --with-pmix=/opt/pmix/v3' --with=hwlocs --with=lua -ta ${SLURM_PKG}
 }
 
 function install_pmix() {
@@ -127,133 +96,5 @@ function install_pmix() {
     cd ../../install/v3/
 }
 
-build_slurm ${1} ${2}#!/bin/bash -e
-set +x
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd ~/
-
-function build_slurm() {
-    set -e
-
-    DISTRO_FAMILY=${1}
-    SLURM_VERSION=${2}
-
-    SLURM_FOLDER="slurm-${SLURM_VERSION}"
-    SLURM_PKG="slurm-${SLURM_VERSION}.tar.bz2"
-    DOWNLOAD_URL="https://download.schedmd.com/slurm"
-
-    if [ "$SLURM_VERSION" \> "20" ]; then
-        PYTHON=python3
-    else
-        PYTHON=python2
-    fi
-
-    case ${DISTRO_FAMILY} in
-        suse)
-            zypper install --no-confirm bzip2 rpmbuild munge-devel pam-devel mysql-devel autoconf readline-devel
-            ;;
-        centos)
-            CENTOS_VERSION=$(cat /etc/centos-release | sed 's/ /\n/g' | grep -E '[0-9.]+')
-            CENTOS_MAJOR=$(echo $CENTOS_VERSION | cut -d. -f1)
-
-            if [ $CENTOS_VERSION \< "8." ]; then
-                LIBTOOL=libtool-2.4.2
-            else
-                LIBTOOL=libtool
-                yum install -y dnf-plugins-core
-                dnf config-manager --set-enabled powertools
-                yum install -y make
-            fi
-
-             # munge is in EPEL
-            yum -y install epel-release && yum -q makecache
-            yum install -y make $PYTHON which rpm-build munge-devel munge-libs readline-devel openssl openssl-devel pam-devel perl-ExtUtils-MakeMaker gcc mysql mysql-devel wget gtk2-devel.x86_64 glib2-devel.x86_64 $LIBTOOL m4 automake rsync
-            ;;
-    esac
-
-    if [ ! -e ~/bin ]; then
-        mkdir -p ~/bin
-    fi
-    
-    install_pmix ${DISTRO_FAMILY}
-
-    cd ~/
-    
-    WHICH_PYTHON=$(which ${PYTHON})
-    if ! [[ ${WHICH_PYTHON} ]]; then
-        ln -s ${WHICH_PYTHON} ~/bin/python
-        export PATH=$PATH:~/bin
-    fi
-
-    if [[ ! -f ${SLURM_PKG} ]]; then
-        wget "${DOWNLOAD_URL}/${SLURM_PKG}"
-    fi
-
-    rpmbuild --with mysql -ta ${SLURM_PKG}
-
-    # make the plugin
-    rm -rf ~/job_submit
-    mkdir -p ~/job_submit
-    cd ~/
-    cd ~/job_submit
-    tar xjf ~/${SLURM_PKG}
-    mkdir -p ${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    cd ${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    rsync -a ${SCRIPT_DIR}/JobSubmitPlugin/ .
-
-    if [ "$SLURM_VERSION" \> "19" ]; then
-        mv Makefile.in.v19 Makefile.in
-    fi
-
-    cd ~/job_submit
-    sed -i 's/src\/plugins\/job_submit\/Makefile/src\/plugins\/job_submit\/Makefile\n                 src\/plugins\/job_submit\/cyclecloud\/Makefile/g'  ${SLURM_FOLDER}/configure.ac
-    cd ~/job_submit/${SLURM_FOLDER}
-
-    if [ "$SLURM_VERSION" \> "19" ]; then
-        autoconf
-    else
-        ./autogen.sh
-    fi
-
-    ./configure
-    make
-    cd ~/job_submit/${SLURM_FOLDER}/src/plugins/job_submit/cyclecloud/
-    make
-
-    LD_LIBRARY_PATH=/root/job_submit/${SLURM_FOLDER}/src/api/.libs/ JOB_SUBMIT_CYCLECLOUD=1 python3 job_submit_cyclecloud_test.py
-    cp .libs/job_submit_cyclecloud.so job_submit_cyclecloud_${DISTRO_FAMILY}${CENTOS_MAJOR}_${SLURM_VERSION}-1.so
-    cp job_submit_cyclecloud_${DISTRO_FAMILY}${CENTOS_MAJOR}_${SLURM_VERSION}-1.so  /usr/src/packages/RPMS/x86_64/
-}
-
-function install_pmix() {
-    case ${1} in
-        suse)
-            zypper install --no-confirm libtool git autoconf flex libevent-devel
-            ;;
-        centos)
-            yum -y install autoconf flex libevent-devel git
-    esac
-   
-    cd ~/
-    mkdir -p /opt/pmix/v3
-    mkdir -p pmix/build/v3 pmix/install/v3
-    cd pmix
-
-    if ! [[ -d source ]]; then
-        git clone https://github.com/openpmix/openpmix.git source
-    fi
-
-    cd source/
-    # git branch -a
-    git checkout v3.1
-    git pull
-    ./autogen.sh
-    cd ../build/v3/
-    ../../source/configure --prefix=/opt/pmix/v3
-    make -j install >/dev/null
-    cd ../../install/v3/
-}
-
-build_slurm ${1} ${2}
+build_slurm ${1} 22.05.8
+build_slurm ${1} 23.02.0
