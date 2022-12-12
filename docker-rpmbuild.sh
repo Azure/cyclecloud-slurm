12bcdcb0f3663fe0d61986eaf2f296d0d27165f3
Ryan Hamel <ryan.hamel@microsoft.com>
2022-12-12 17:08:45 -0500

    Use 22.05.8  and 23.02

--- docker-rpmbuild.sh
+++ docker-rpmbuild.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+set -e
+SOURCE_ROOT=$( realpath $(dirname $0) )
+cd $(dirname $0)/slurm/install
 
-TMP_BLOBS=blobs-tmp
-rm -rf $TMP_BLOBS
-mkdir $TMP_BLOBS
+if [ ! -e slurm-pkgs ]; then 
+  mkdir slurm-pkgs
+fi
 
+TMP_BINS=slurm-pkgs-tmp
+rm -rf $TMP_BINS
+mkdir $TMP_BINS
 
 if command -v docker; then
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/blobs:/root/rpmbuild/RPMS/x86_64 -ti almalinux:8.5 /bin/bash /source/00-build-slurm.sh centos
-  
-  # we need to create centos7 rpms to create the valid debs for Ubuntu18
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/$TMP_BLOBS:/root/rpmbuild/RPMS/x86_64 -ti centos:7 /bin/bash /source/00-build-slurm.sh centos
-  docker run -v $(pwd)/specs/default/cluster-init/files:/source -v $(pwd)/$TMP_BLOBS:/root/rpmbuild/RPMS/x86_64 -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
-  
-  mv $TMP_BLOBS/* blobs/
-  rm -rf $TMP_BLOBS
-  cat > /tmp/check_mising_files.py <<EOF
-from configparser import ConfigParser
-import os
-parser = ConfigParser()
-parser.read("project.ini") 
-expected_files = set([x.strip() for x in parser.get("blobs", "Files").split(",")])
-actual = set(os.listdir("blobs"))
-missing = expected_files - actual
-assert not missing, "Missing the following blobs: %s" % ",".join(missing)
-EOF
-  python3 /tmp/check_mising_files.py
+  runtime=docker
+  extra_args=
+elif command -v podman; then
+  runtime=podman
+  extra_args="--privileged"
 else
-  echo "`docker` binary not found. Install docker to build RPMs with this script"
+  echo "docker binary not found. Install docker to build RPMs with this script"
+  exit 1
 fi
+
+$runtime run -v $SOURCE_ROOT/specs/default/cluster-init/files:/source -v $SOURCE_ROOT/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 $extra_args -ti almalinux:8.5 /bin/bash /source/00-build-slurm.sh centos
+$runtime run -v $SOURCE_ROOT/specs/default/cluster-init/files:/source -v $SOURCE_ROOT/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 -ti centos:7 /bin/bash /source/00-build-slurm.sh centos
+$runtime run -v $SOURCE_ROOT/specs/default/cluster-init/files:/source -v $SOURCE_ROOT/$TMP_BINS:/root/rpmbuild/RPMS/x86_64 $extra_args -ti ubuntu:18.04 /bin/bash /source/01-build-debs.sh
+
+mv $TMP_BINS/* slurm-pkgs/
+rm -rf $TMP_BINS
+
--- sbin/suspend_program.sh
+++ sbin/suspend_program.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+node_list=$(echo $@ | sed "s/ /,/g")
 source /opt/azurehpc/slurm/venv/bin/activate
-azslurm suspend --node-list $1
+azslurm suspend --node-list $node_list
 exit $?
