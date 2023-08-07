cd12bdcccc66c2eb4f6fa0e1cc078b040dad3996
Aditi Gaur <116667879+aditigaur4@users.noreply.github.com>
2023-08-07 16:04:43 -0700

    Prolog slurmctld script needs to use correct binaries. (#146)
    Slurm binaries can be present under /bin or /usr/bin.
    
    Signed-off-by: Aditi Gaur <aditigaur@microsoft.com>

--- sbin/prolog.sh
+++ sbin/prolog.sh
@@ -3,8 +3,19 @@ set -x
 
 log=/var/log/slurmctld/prolog_slurmctld.log
 script=/opt/azurehpc/slurm/get_acct_info.sh
-scontrol=/bin/scontrol
-JQ=/bin/jq
+
+if [ -e /bin/scontrol ]; then
+	scontrol=/bin/scontrol
+elif [ -e /usr/bin/scontrol ]; then
+	scontrol=/usr/bin/scontrol
+fi
+
+if [ -e /bin/jq ]; then
+	JQ=/bin/jq
+elif [ -e /usr/bin/jq ]; then
+	JQ=/usr/bin/jq
+fi
+
 job=$SLURM_JOBID
 
 nodename=$($scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
