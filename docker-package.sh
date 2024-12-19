e381a035b04c0f759640032933775544cca3bbff
Aditi Gaur <116667879+aditigaur4@users.noreply.github.com>
2024-12-18 18:02:59 -0800

    Fix docker_package.sh to exit if working directory is dirty. (#299)
    Also includes fixes for better readability.
    Script will exit if any uncommitted changes are present.
    Script will also delete pre-existing blobs.

--- docker-package.sh
+++ docker-package.sh
@@ -1,5 +1,60 @@
 #!/bin/bash
 
+log_file=".build.log"
+check_dirty_changes() {
+  if [ -n "$(git status --porcelain)" ]; then
+    echo "Error: There are uncommitted changes in the current branch. Please commit or stash them before running this script."
+    exit 1
+  fi
+}
+
+# Call the function to check for dirty changes
+check_dirty_changes
+
+print_branch_and_last_commit() {
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  last_commit=$(git log -1 --pretty=format:"%h - %s (%ci)")
+
+  printf "%-20s: %s\n" "Current branch" "$branch"
+  printf "%-20s: %s\n" "Last commit" "$last_commit"
+}
+
+delete_existing_blobs() {
+  printf "%-20s: %s\n" "Deleting existing Blob Files" ""
+  while IFS= read -r file; do
+    if [ -f "blobs/$file" ]; then
+      printf "%-20s: %s\n" "" "$file"
+      rm -f "blobs/$file"
+    fi
+  done < <(awk -F' *= *' '/^\[blobs\]/ {found=1} found && /^Files/ {gsub(/, */, "\n", $2); print $2; exit}' project.ini)
+}
+
+check_blobs_files_exist() {
+  local version="$1"
+  local missing_files=0
+
+  printf "%-20s: %s\n" "Blob Files" ""
+  while IFS= read -r file; do
+    printf "%-20s: %s\n" "" "$file"
+    if [ ! -f "blobs/$file" ]; then
+      echo "Error: File blobs/$file does not exist."
+      missing_files=1
+    fi
+  done < <(awk -F' *= *' '/^\[blobs\]/ {found=1} found && /^Files/ {gsub(/, */, "\n", $2); print $2; exit}' project.ini)
+
+  if [ $missing_files -eq 1 ]; then
+    echo "One or more required files are missing in the blobs directory."
+    exit 1
+
+  fi
+}
+
+get_version_from_project_ini() {
+  version=$(awk -F' *= *' '/^\[project\]/ {found=1} found && /^version/ {print $2; exit}' project.ini)
+  printf "%-20s: %s\n" "Project Version" "$version"
+  check_blobs_files_exist "$version"
+}
+
 local_azslurm=/source/azure-slurm
 if [ "$1" != "" ]; then
   scalelib=$(realpath $1)
@@ -18,6 +73,13 @@ else
   exit 1
 fi
 
-# allows caching
-$runtime build -t azslurm_build:latest -f util/Dockerfile .
-$runtime run -v $(pwd):${local_azslurm} $runtime_args $extra_args -ti azslurm_build:latest /bin/bash ${local_azslurm}/util/build.sh $local_scalelib
+{
+  delete_existing_blobs
+  # allows caching
+  $runtime build -t azslurm_build:latest -f util/Dockerfile .
+  $runtime run -v $(pwd):${local_azslurm} $runtime_args $extra_args -ti azslurm_build:latest /bin/bash ${local_azslurm}/util/build.sh $local_scalelib
+} &> $log_file
+
+# Call the function to print the branch and the last commit
+print_branch_and_last_commit
+get_version_from_project_ini
