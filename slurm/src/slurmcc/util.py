aa48d6a8cb72713737310c946951810667899ac9
Azreen Zaman <azreenzaman@microsoft.com>
2025-02-14 12:36:31 -0500

    setting shell default to False

--- slurm/src/slurmcc/util.py
+++ slurm/src/slurmcc/util.py
@@ -41,7 +41,7 @@ class NativeSlurmCLIImpl(NativeSlurmCLI):
             return retry_subprocess(lambda: check_output(full_args)).strip()
         return check_output(full_args).strip()
 
-    def srun(self, hostlist: List[str], user_command: str, timeout: int, shell=True) -> SrunOutput:
+    def srun(self, hostlist: List[str], user_command: str, timeout: int, shell=False) -> SrunOutput:
         with tempfile.NamedTemporaryFile(delete=True) as temp_file:
             temp_file_path = temp_file.name
 
