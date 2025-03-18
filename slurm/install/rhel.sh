2b62eefae0b7d6d68fb0401cdf3cf694a4ab9ea5
azreenz <azreenzaman@microsoft.com>
2025-03-18 15:34:45 -0400

    remove download blobs from github (#336)
    * remove download blobs from github
    
    * remove disable_pmc option from scripts and templates

--- slurm/install/rhel.sh
+++ slurm/install/rhel.sh
@@ -10,17 +10,12 @@ fi
 
 SLURM_ROLE=$1
 SLURM_VERSION=$2
-DISABLE_PMC=$3
 OS_VERSION=$(cat /etc/os-release  | grep VERSION_ID | cut -d= -f2 | cut -d\" -f2 | cut -d. -f1)
 OS_ID=$(cat /etc/os-release  | grep ^ID= | cut -d= -f2 | cut -d\" -f2 | cut -d. -f1)
 
 if [ "$OS_VERSION" -lt "8" ]; then
     echo "RHEL versions < 8 no longer supported"
     exit 1
-elif [ "$OS_VERSION" -gt "8" ]; then
-    PACKAGE_DIR=slurm-pkgs-rhel9
-else
-    PACKAGE_DIR=slurm-pkgs-rhel8
 fi
 
 if [ "${OS_ID,,}" == "rhel" ]; then
@@ -36,55 +31,34 @@ sched_packages="slurm-slurmctld slurm-slurmdbd"
 execute_packages="slurm-slurmd"
 
 
-if [ "$DISABLE_PMC" == "False" ]; then
-
-    if [ "$OS_VERSION" -gt "8" ]; then
-        cp slurmel9.repo /etc/yum.repos.d/slurm.repo
-    else
-        cp slurmel8.repo /etc/yum.repos.d/slurm.repo
-    fi
-
-    ## This package is pre-installed in all hpc images used by cyclecloud, but if customer wants to
-    ## build an image from generic marketplace images then this package sets up the right gpg keys for PMC.
-    if [ ! -e /etc/yum.repos.d/microsoft-prod.repo ];then
-        curl -sSL -O https://packages.microsoft.com/config/rhel/$OS_VERSION/packages-microsoft-prod.rpm
-        rpm -i packages-microsoft-prod.rpm
-        rm packages-microsoft-prod.rpm
-    fi
-    for pkg in $slurm_packages; do
-        yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
-    done
-    if [ ${SLURM_ROLE} == "scheduler" ]; then
-        for pkg in $sched_packages; do
-            yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
-        done
-    fi
-
-    if [ ${SLURM_ROLE} == "execute" ]; then
-        for pkg in $execute_packages; do
-            yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
-        done
-    fi
-    touch $INSTALLED_FILE
-
-    exit
+if [ "$OS_VERSION" -gt "8" ]; then
+    cp slurmel9.repo /etc/yum.repos.d/slurm.repo
+else
+    cp slurmel8.repo /etc/yum.repos.d/slurm.repo
 fi
 
-
+## This package is pre-installed in all hpc images used by cyclecloud, but if customer wants to
+## build an image from generic marketplace images then this package sets up the right gpg keys for PMC.
+if [ ! -e /etc/yum.repos.d/microsoft-prod.repo ];then
+    curl -sSL -O https://packages.microsoft.com/config/rhel/$OS_VERSION/packages-microsoft-prod.rpm
+    rpm -i packages-microsoft-prod.rpm
+    rm packages-microsoft-prod.rpm
+fi
 for pkg in $slurm_packages; do
-    yum -y install $PACKAGE_DIR/slurm-$SLURM_VERSION/RPMS/$pkg-${SLURM_VERSION}.el${OS_VERSION}*.rpm
+    yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
 done
-
 if [ ${SLURM_ROLE} == "scheduler" ]; then
     for pkg in $sched_packages; do
-        yum  -y install $PACKAGE_DIR/slurm-$SLURM_VERSION/RPMS/$pkg-${SLURM_VERSION}.el${OS_VERSION}*.rpm
+        yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
     done
 fi
 
 if [ ${SLURM_ROLE} == "execute" ]; then
     for pkg in $execute_packages; do
-        yum  -y install $PACKAGE_DIR/slurm-$SLURM_VERSION/RPMS/$pkg-${SLURM_VERSION}.el${OS_VERSION}*.rpm
+        yum -y install $pkg-${SLURM_VERSION}.el${OS_VERSION} --disableexcludes slurm
     done
 fi
-
 touch $INSTALLED_FILE
+
+exit
+
