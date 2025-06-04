11484eb198946c64dec34c4354678238e96ca627
Azreen Zaman <azreenzaman@microsoft.com>
2025-06-04 13:57:33 -0400

    change cli argument to --viz

--- azure-slurm/slurmcc/cli.py
+++ azure-slurm/slurmcc/cli.py
@@ -197,10 +197,10 @@ class SlurmCLI(CommonCLI):
         topology_group.add_argument('-b', '--block', action='store_true', default=False, help='Generate Block Topology output to use Block topology plugin (default: False)')
         topology_group.add_argument('-t', '--tree', action='store_true', default=False, help='Generate Tree Topology output to use Tree topology plugin(default: False)')
         parser.add_argument("-s", "--block_size", type=int, required=False, help="Minimum block size required for each block (use with --block or --use_nvlink_domain, default: 1)")
-        parser.add_argument("-g", "--generate_visualization", action='store_true', default=False, help="Generate ASCII visualization for the topology (default: False)")
+        parser.add_argument("--viz", action='store_true', default=False, help="Generate ASCII visualization for the topology (default: False)")
         parser.add_argument("--visual_block_size", type=int, default=18, help="Block size for visualization (default: 18)")
 
-    def topology(self, config: Dict, partition, output, use_vmss, use_fabric_manager, use_nvlink_domain, tree, block, block_size, generate_visualization, visual_block_size) -> None:
+    def topology(self, config: Dict, partition, output, use_vmss, use_fabric_manager, use_nvlink_domain, tree, block, block_size, viz, visual_block_size) -> None:
         """
         Generates Topology Plugin Configuration
         """
@@ -213,7 +213,7 @@ class SlurmCLI(CommonCLI):
             config_dir = config.get("config_dir")
             topo = topology.Topology(partition, output, topology.TopologyInput.FABRIC, topo_type, config_dir)
             content = topo.run()
-            if generate_visualization:
+            if viz:
                 print(topo.visualize(content))
         elif use_nvlink_domain:
             if not partition:
@@ -225,11 +225,11 @@ class SlurmCLI(CommonCLI):
             config_dir = config.get("config_dir")
             topo = topology.Topology(partition, output, topology.TopologyInput.NVLINK, topo_type, config_dir, block_size)
             content = topo.run()
-            if generate_visualization:
+            if viz:
                 print(topo.visualize(content, max_block_size=visual_block_size))
         elif use_vmss:
-            if block or block_size or partition or generate_visualization:
-                raise ValueError("--block, --block_size, --partition, and --generate_visualization are not supported with --use_vmss")
+            if block or block_size or partition or viz:
+                raise ValueError("--block, --block_size, --partition, and --viz are not supported with --use_vmss")
             if output:
                 with open(output, 'w', encoding='utf-8') as file_writer:
                     return _generate_topology(self._get_node_manager(config), file_writer)
