5daa6242170d259e76e1e586a2c1916e8bfd1e0e
ryanhamel <ryhamel@microsoft.com>
2025-06-23 19:41:15 -0400

    Large cluster bugfix: max arg limit reached (#376)
    * implement srun implementation to mock cli
    
    * Port max args fix from 4.0.0 implementation

--- slurm/test/slurmcc_test/util_test.py
+++ slurm/test/slurmcc_test/util_test.py
@@ -1,4 +1,5 @@
-from slurmcc.util import to_hostlist, get_sort_key_func
+from slurmcc.util import to_hostlist, get_sort_key_func, run, _show_nodes, set_slurm_cli, _from_hostlist, _to_hostlist
+from slurmcc_test.testutil import MockNativeSlurmCLI
 from typing import List
 
 def scontrol_func(args: List[str], retry: bool = True) -> str:
@@ -35,3 +36,54 @@ def test_get_sort_key_func() -> None:
 def test_to_hostlist() -> None:
     assert "name-1,dyn" == to_hostlist(["name-1","dyn"], scontrol_func)
     assert "name-1,dyn" == to_hostlist(["dyn","name-1"], scontrol_func)
+
+
+def test_show_nodes() -> None:
+    # no differences based on splitting
+    cli = MockNativeSlurmCLI()
+    node_list = ["htc-1", "htc-2", "htc-3", "htc-4"]
+    set_slurm_cli(cli)
+    cli.create_nodes(node_list, ["cloud"], ["htc"])
+    complete = _show_nodes(node_list, 4)
+    split = _show_nodes(node_list, 2)
+    assert split == complete
+
+
+def test_from_hostlist() -> None:
+    # becomes so large we actually can't express htc-[min-max]
+    # so the final result is actually different
+    cli = MockNativeSlurmCLI()
+    node_list = ["htc-1", "htc-2", "htc-3", "htc-4"]
+    set_slurm_cli(cli)
+    def simple_scontrol(args, ignore):
+        assert args[0] == "show"
+        assert args[1] == "hostnames"
+        if args[2] == "htc-1,htc-2":
+            return "htc-[1-2]"
+        if args[2] == "htc-3,htc-4":
+            return "htc-[3-4]"
+        if args[2] == "htc-1,htc-2,htc-3,htc-4":
+            return "htc-[1-4]"
+        raise RuntimeError(args)
+        
+    
+    cli.create_nodes(node_list, ["cloud"], ["htc"])
+    cli.scontrol = simple_scontrol
+    complete = _from_hostlist(",".join(node_list), 4)
+    split = _from_hostlist(",".join(node_list), 2)
+    assert split != complete
+    assert complete == ["htc-[1-4]"]
+    assert split == ["htc-[1-2]", "htc-[3-4]"]
+
+
+def test_to_hostlist() -> None:
+    # no changes based on splitting
+    # confirmed this caused a failure with over 2k nodes
+    # dropping to 500 fixed the issue
+    cli = MockNativeSlurmCLI()
+    node_list = ["htc-1", "htc-2", "htc-3", "htc-4"]
+    set_slurm_cli(cli)
+    cli.scontrol = scontrol_func  # already implemented fake to hostlist
+    complete = _to_hostlist(node_list, max_nodes_in_list=4)
+    split = _to_hostlist(node_list, max_nodes_in_list=2)
+    assert split == complete
