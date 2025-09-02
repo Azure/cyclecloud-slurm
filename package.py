2be554d06f8d63a01a15a2e8317d3942ad11d77a
ryanhamel <ryhamel@microsoft.com>
2025-09-02 17:11:51 -0400

    Use scalelib 1.0.7 (#385) (#413)

--- package.py
+++ package.py
@@ -11,7 +11,7 @@ from argparse import Namespace
 from subprocess import check_call
 from typing import Dict, List, Optional
 
-SCALELIB_VERSION = "1.0.6"
+SCALELIB_VERSION = "1.0.7"
 CYCLECLOUD_API_VERSION = "8.7.1"
 
 
