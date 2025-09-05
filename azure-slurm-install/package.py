75fe0d88fb68724ab67da266e92900e4dbf684d8
Azreen  Zaman <azreenzaman@microsoft.com>
2025-09-05 11:51:35 -0400

    Bump default slurm version to 25.05.2 (#408)
    * pull from insiders if slurm.insiders=True for rhel.sh
    
    * bump slurm version to 25.05.2-1
    
    * return False if slurm.insiders is not defined
    
    * get rid of comment in package.py
    
    * if slurm.insiders=True pull from insiders repo for ubuntu.sh
    
    * set slurm.insiders = false in template
    
    * close if statement
    
    * copy repos for only rhel8 or 9 exit on anything else

--- azure-slurm-install/package.py
+++ azure-slurm-install/package.py
@@ -46,6 +46,8 @@ def execute() -> None:
     _add("install_logging.conf", "conf/install_logging.conf")
     _add("installlib.py", "installlib.py")
     _add("install.py", "install.py")
+    _add("slurmel8insiders.repo", "slurmel8insiders.repo")
+    _add("slurmel9insiders.repo", "slurmel9insiders.repo")
     _add("slurmel8.repo", "slurmel8.repo")
     _add("slurmel9.repo", "slurmel9.repo")
     _add("ubuntu.sh", "ubuntu.sh", 600)
--- azure-slurm-install/rhel.sh
+++ azure-slurm-install/rhel.sh
@@ -30,11 +30,23 @@ slurm_packages="slurm slurm-libpmi slurm-devel slurm-pam_slurm slurm-perlapi slu
 sched_packages="slurm-slurmctld slurm-slurmdbd slurm-slurmrestd"
 execute_packages="slurm-slurmd"
 
+INSIDERS=$(/opt/cycle/jetpack/bin/jetpack config slurm.insiders False)
 
-if [ "$OS_VERSION" -gt "8" ]; then
-    cp slurmel9.repo /etc/yum.repos.d/slurm.repo
+if [[ "$OS_VERSION" == "9" ]]; then
+    if [[ "$INSIDERS" == "True" ]]; then
+        cp slurmel9insiders.repo /etc/yum.repos.d/slurm.repo
+    else
+        cp slurmel9.repo /etc/yum.repos.d/slurm.repo
+    fi
+elif [[ "$OS_VERSION" == "8" ]]; then
+    if [[ "$INSIDERS" == "True" ]]; then
+        cp slurmel8insiders.repo /etc/yum.repos.d/slurm.repo
+    else
+        cp slurmel8.repo /etc/yum.repos.d/slurm.repo
+    fi
 else
-    cp slurmel8.repo /etc/yum.repos.d/slurm.repo
+    echo "Unsupported OS version: $OS_VERSION"
+    exit 1
 fi
 
 ## This package is pre-installed in all hpc images used by cyclecloud, but if customer wants to
