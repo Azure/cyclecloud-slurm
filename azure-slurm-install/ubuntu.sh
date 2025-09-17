5c4eae21f53402b25cdb84f912fb0d5177c447ab
Azreen Zaman <azreenzaman@microsoft.com>
2025-09-17 15:19:04 -0400

    remove unused code

--- azure-slurm-install/ubuntu.sh
+++ azure-slurm-install/ubuntu.sh
@@ -126,26 +126,3 @@ dpkg_pkg_install "$all_packages"
 
 touch $INSTALLED_FILE
 exit
-
-if [[ $UBUNTU_VERSION > "19" ]]; then
-    apt install -y libhwloc15
-
-    ln -sf /lib/x86_64-linux-gnu/libreadline.so.8 /usr/lib/x86_64-linux-gnu/libreadline.so.7
-    ln -sf /lib/x86_64-linux-gnu/libhistory.so.8 /usr/lib/x86_64-linux-gnu/libhistory.so.7
-    ln -sf /lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.6
-    ln -sf /lib/x86_64-linux-gnu/libtinfo.so.6.2 /usr/lib/x86_64-linux-gnu/libtinfo.so.6
-    # part of what is needed for perl support, but libperl.so.5.26 is not available on ubuntu via public
-    # repos, so for this release we will need to have support use cloud-init to install perl
-    ln -sf /usr/lib64/libslurm.so.38 /usr/lib/x86_64-linux-gnu/
-
-else
-    ln -sf /usr/lib/x86_64-linux-gnu/libssl.so /usr/lib/x86_64-linux-gnu/libssl.so.10 
-    ln -sf /usr/lib/x86_64-linux-gnu/libcrypto.so /usr/lib/x86_64-linux-gnu/libcrypto.so.10
-    ln -sf /usr/lib/x86_64-linux-gnu/libmysqlclient.so /usr/lib/x86_64-linux-gnu/libmysqlclient.so.18
-    # Need to manually create links for libraries the RPMs are linked to (we use alien to create the debs)
-
-    ln -sf /lib/x86_64-linux-gnu/libreadline.so.7 /usr/lib/x86_64-linux-gnu/libreadline.so.6 
-    ln -sf /lib/x86_64-linux-gnu/libhistory.so.7 /usr/lib/x86_64-linux-gnu/libhistory.so.6
-    ln -sf /lib/x86_64-linux-gnu/libncurses.so.5 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-    ln -sf /lib/x86_64-linux-gnu/libtinfo.so.5 /usr/lib/x86_64-linux-gnu/libtinfo.so.5
-fi
