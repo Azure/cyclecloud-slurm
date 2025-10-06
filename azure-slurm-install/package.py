0d4b72a19c1154d5b8b0bf83cc5ce3ea6581e336
Azreen Zaman <azreenzaman@microsoft.com>
2025-10-06 16:47:22 -0400

    refactor

--- azure-slurm-install/package.py
+++ azure-slurm-install/package.py
@@ -32,23 +32,15 @@ def execute() -> None:
         f"dist/azure-slurm-install-pkg-{version}.tar.gz", "w"
     )
 
-    def _download_file(url: str, dest: str) -> None:
-        response = requests.get(url, stream=True)
+    def _download(url: str, dest: str) -> None:
+        response = requests.get(url, stream=True, timeout=60)
         response.raise_for_status()
         with open(dest, "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
+
     artifacts_dir = "artifacts"
     os.makedirs(artifacts_dir, exist_ok=True)
-
-    epel8_url = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
-    epel9_url = "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
-
-    epel8_dest = os.path.join(artifacts_dir, "epel-release-latest-8.noarch.rpm")
-    epel9_dest = os.path.join(artifacts_dir, "epel-release-latest-9.noarch.rpm")
-
-    _download_file(epel8_url, epel8_dest)
-    _download_file(epel9_url, epel9_dest)
 
     def _add(name: str, path: Optional[str] = None, mode: Optional[int] = None) -> None:
         path = path or name
@@ -61,6 +53,12 @@ def execute() -> None:
         with open(path, "rb") as fr:
             tf.addfile(tarinfo, fr)
 
+    epel_versions = ["8", "9"]
+    for ver in epel_versions:
+        url = f"https://dl.fedoraproject.org/pub/epel/epel-release-latest-{ver}.noarch.rpm"
+        dest = os.path.join(artifacts_dir, f"epel-release-latest-{ver}.noarch.rpm")
+        _download(url, dest)
+        _add(dest, dest)
     _add("install.sh", "install.sh", mode=os.stat("install.sh")[0])
     _add("install_logging.conf", "conf/install_logging.conf")
     _add("installlib.py", "installlib.py")
@@ -77,8 +75,6 @@ def execute() -> None:
     _add("suse.sh", "suse.sh", 600)
     _add("start-services.sh", "start-services.sh", 555)
     _add("capture_logs.sh", "capture_logs.sh", 755)
-    _add(epel8_dest, epel8_dest)
-    _add(epel9_dest, epel9_dest)
 
     for fil in os.listdir("templates"):
         if os.path.isfile(f"templates/{fil}"):
