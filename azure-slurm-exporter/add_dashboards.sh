dc7b8d83d1d2628a3202b1df002f017c68769688
Azreen Zaman <azreenzaman@microsoft.com>
2026-02-19 12:49:33 -0500

    update dashboard json

---
+++ azure-slurm-exporter/add_dashboards.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+RESOURCE_GROUP_NAME=$1
+GRAFANA_NAME=$2 
+
+if [ -z "$GRAFANA_NAME" ]; then
+  echo "Usage: $0 <resource-group-name> <grafana-name>"
+  exit 1
+fi
+if [ -z "$RESOURCE_GROUP_NAME" ]; then
+  echo "Usage: $0 <resource-group-name> <grafana-name>"
+  exit 1
+fi
+
+FOLDER_NAME="Azure CycleCloud"
+DASHBOARD_FOLDER=$THIS_DIR/dashboards
+# Create Grafana dashboards folders
+az grafana folder show -n $GRAFANA_NAME -g $RESOURCE_GROUP_NAME --folder "$FOLDER_NAME" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "$FOLDER_NAME folder does not exist. Creating it."
+  az grafana folder create --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --title "$FOLDER_NAME"
+fi
+
+# Slurm Dashboard
+az grafana dashboard import --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --folder "$FOLDER_NAME" --overwrite true --definition $DASHBOARD_FOLDER/slurm.json
+az grafana dashboard import --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --folder "$FOLDER_NAME" --overwrite true --definition $DASHBOARD_FOLDER/failed-jobs.json
