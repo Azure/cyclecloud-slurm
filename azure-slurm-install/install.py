3b422c59c7bc384e0bc11313a585d0fd011160ad
Azreen Zaman <azreenzaman@microsoft.com>
2026-04-22 11:45:50 -0400

    configure slurmd and slumrestd in /etc/sysconfig

--- azure-slurm-install/install.py
+++ azure-slurm-install/install.py
@@ -824,7 +824,7 @@ def setup_slurmd(s: InstallSettings) -> None:
             slurmd_config = slurmd_config + " -b"
 
     ilib.file(
-        "/etc/sysconfig/slurmd" if s.platform_family == "rhel" else "/etc/default/slurmd",
+        "/etc/sysconfig/slurmd" if s.platform_family in ["rhel", "azurelinux"] else "/etc/default/slurmd",
         content=slurmd_config,
         owner=s.slurm_user,
         group=s.slurm_grp,
@@ -862,7 +862,7 @@ def setup_slurmrestd(s: InstallSettings) -> None:
     openapi_flag = "" if s.acct_enabled else " -s openapi/slurmctld"
     slurmrestd_config = f"SLURMRESTD_OPTIONS=\"-u slurmrestd -g slurmrestd{openapi_flag}\"\nSLURMRESTD_LISTEN=:6820,unix:/var/spool/slurmrestd/slurmrestd.socket"
     ilib.file(
-        "/etc/sysconfig/slurmrestd" if s.platform_family == "rhel" else "/etc/default/slurmrestd",
+        "/etc/sysconfig/slurmrestd" if s.platform_family in ["rhel", "azurelinux"] else "/etc/default/slurmrestd",
         content=slurmrestd_config,
         owner=s.slurmrestd_user,
         group=s.slurmrestd_grp,
@@ -1192,7 +1192,7 @@ def main() -> None:
     # create the munge key and/or copy it to /etc/munge/
     munge_key(settings)
 
-    # runs either rhel.sh or ubuntu.sh to install the packages
+    # runs either rhel.sh, ubuntu.sh, or azurelinux.sh to install packages or verify the packages are installed
     run_installer(settings, os.path.abspath(f"{args.platform}.sh"), args.mode)
 
     #check for libjwt to configure JWT auth for slurmrestd
